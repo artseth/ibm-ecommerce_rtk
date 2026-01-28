c991bbd595800189e50eaec7d209f6ba6619a47e
artseth <artsethmedia01@gmail.com>
2026-01-28 21:39:47 +0200

    ShoppingCart2.jsx

---
+++ src/Components/ShoppingCart2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './ShoppingCart.css';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
+
+const dispatch = useDispatch();
+const cartItems = useSelector(state => state.cart.cartItems)
+const totalAmount = cartItems.reduce ((total, item) => total + item.price * item.quantity, 0);
+
+const ShoppingCart = () => {
+
+    return(
+        <>
+            <div className='shopping-cart'>
+                <h2 className='shopping-cart-title'>Shopping Cart</h2>
+                <ul className='cart-items'>
+
+                </ul>
+                <button className='clear-cart-btn'>Clear Cart</button>
+
+            </div>
+        </>
+    )
+}
